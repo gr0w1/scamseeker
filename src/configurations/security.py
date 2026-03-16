b413fa878ce0608a37e6639d65ede652ab6cd7a4
kkkreks <katebulygina2001@gmail.com>
2026-03-16 18:07:20 +0300

    update

--- src/configurations/security.py
+++ src/configurations/security.py
@@ -3,7 +3,7 @@ from typing import Optional
 
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
-from jose import JWTError, jwte
+from jose import JWTError, jwt
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.configurations.settings import settings
